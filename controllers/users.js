04075b4ec67e3507920c5812bafe78d19cf40558
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-05 16:26:03 -0600

    Added sign in page to routes. Needed to downgrade 'validator' due to issue with latest version.

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,15 @@ module.exports = function(_) {
   return {
     SetRouting: function(router) {
       router.get('/', this.indexPage);
+      router.get('/signup', this.getSignUp);
     },
 
     indexPage: function(req, res) {
       return res.render('index', { test: 'Everything is shiny captain!' });
+    },
+
+    getSignUp: function(req, res) {
+      return res.render('signup');
     }
   };
 };
--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('flash');
+const passport = require('passport');
+
 const container = require('./container');
 
 container.resolve(function(users) {
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://localhost/wrestlechat');
   const app = SetupExpress();
 
   function SetupExpress() {
@@ -24,8 +34,24 @@ container.resolve(function(users) {
 
   function ConfigureExpress(app) {
     app.use(express.static('public'));
+    app.use(cookieParser());
     app.set('view engine', 'ejs');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(validator());
+    app.use(
+      session({
+        secret: 'itisasecrettoeveryone',
+        resave: true,
+        saveIntitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+      })
+    );
+
+    app.use(flash());
+    // make sure these files are loaded after 'session'
+    app.use(passport.initialize());
+    app.use(passport.session());
   }
 });
