46f18ffdf5444fe38afa82b4dad36ea66e9eab07
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-09 14:26:55 -0500

    Adding google passport

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ container.resolve(function(users, _) {
 
   function ConfigureExpress(app) {
     require('./passport/passport-local');
+    require('./passport/passport-facebook');
+    require('./passport/passport-google');
 
     app.use(express.static('public'));
     app.use(cookieParser());
