826c284b72c2d42bd188f70a0a2b73b6070dfe59
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-19 14:17:51 -0500

    Added async Allowing cycle through DB for show names Sorting through country and sort alpha from DB

--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,45 @@ module.exports = function(async, show, _) {
       router.get('/home', this.homePage);
     },
     homePage: function(req, res) {
-      return res.render('home');
+      async.parallel(
+        [
+          function(callback) {
+            show.find({}, (err, result) => {
+              callback(err, result);
+            });
+          },
+          function(callback) {
+            show.aggregate(
+              {
+                $group: {
+                  _id: '$country'
+                }
+              },
+              (err, newResult) => {
+                callback(err, newResult);
+              }
+            );
+          }
+        ],
+        (err, results) => {
+          const res1 = results[0];
+          const res2 = results[1];
+
+          const dataChunk = [];
+          const chunkSize = 3;
+          for (let i = 0; i < res1.length; i += chunkSize) {
+            dataChunk.push(res1.slice(i, i + chunkSize));
+          }
+
+          const countrySort = _.sortBy(res2, '_id');
+
+          res.render('home', {
+            title: 'Wrestlechat - Home',
+            data: dataChunk,
+            country: countrySort
+          });
+        }
+      );
     }
   };
 };
